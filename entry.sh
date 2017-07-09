c2c43904f92e09dda6d12af9c4a4d35cccb89977
stackdump <myork@stackdump.com>
2017-07-09 10:28:52 -0500

    WIP

--- entry.sh
+++ entry.sh
@@ -5,5 +5,9 @@ if [[ "x${RDS_HOST}" = 'x'  && "x${RDS_PORT_5432_TCP_ADDR}" != "x" ]] ; then
     export RDS_HOST=$RDS_PORT_5432_TCP_ADDR # see: ./run.sh example using --link
 fi
 
+if [[ "x${PNML_PATH}" = 'x' ]] ; then
+    export PNML_PATH=./schemata
+fi
+
 export PYTHONPATH=./
 twistd -n bitwrap --listen-ip=0.0.0.0 --listen-port=8080
--- schemata/README.md
+++ schemata/README.md
@@ -2,12 +2,3 @@
 
 This directory contains from example Petri-Net definitions created with 
 [PIPEv5](https://github.com/sarahtattersall/PIPE)
-
-```
-bitwrap_machine/examples/
-├── counter.xml
-├── microcosm.xml
-├── octoe.xml
-├── README.md
-└── vote.xml
-```
