7ec44bbbd3356726eb7911143e6b99028dc7c52f
stackdump <myork@stackdump.com>
2017-07-22 09:48:56 -0500

    WIP

--- Dockerfile
+++ Dockerfile
@@ -6,10 +6,6 @@ RUN pip install -r requirements.txt
 COPY . .
 RUN pip install .
 
-VOLUME ["/opt/bitwrap", "/repo"]
-
-ENV PNML_PATH=/opt/txbitwrap/schemata
-
 EXPOSE 8080
 
-ENTRYPOINT ["/opt/txbitwrap/entry.sh"]
+ENTRYPOINT ["./entry.sh"]
--- run.sh
+++ run.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-if [[ "${1}x" = 'x' ]] ; then
+if [[ "x${CONTAINER_NAME}" = 'x' ]] ; then
   CONTAINER_NAME='txbitwrap-dev'
-else
-  CONTAINER_NAME=$1
 fi
+
 echo "rebuilding: ${CONTAINER_NAME}"
 
 docker rm --force $CONTAINER_NAME &>/dev/null
@@ -15,4 +14,4 @@ docker run -it --name=${CONTAINER_NAME} \
   --link rabbit-dev:amqp \
   -v ${HOME}:/opt/bitwrap \
   -p 127.0.0.1:8080:8080 \
-  bitwrap/txbitwrap:dev
+  bitwrap/txbitwrap:dev $@
